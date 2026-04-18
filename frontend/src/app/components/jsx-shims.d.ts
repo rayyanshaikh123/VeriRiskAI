b6fdc83e18e197c3457bc05d0d3b30d83dcd2c50
Moksh1112 <pmoksh1112@gmail.com>
2026-04-18 19:14:16 +0530

    gf

--- frontend/src/app/components/jsx-shims.d.ts
+++ frontend/src/app/components/jsx-shims.d.ts
@@ -1,14 +1,14 @@
 declare module "./Threads" {
-  const Threads: any;
-  export default Threads;
+    const Threads: any;
+    export default Threads;
 }
 
 declare module "./SpotlightCard" {
-  const SpotlightCard: any;
-  export default SpotlightCard;
+    const SpotlightCard: any;
+    export default SpotlightCard;
 }
 
 declare module "./BlurText" {
-  const BlurText: any;
-  export default BlurText;
+    const BlurText: any;
+    export default BlurText;
 }
